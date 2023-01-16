4d129bb74f2eb172a1eee942e8c2898954d4da4d
2h5i <dev.2h5i@gmail.com>
2023-01-16 14:49:47 +0900

    프로젝트 시작

---
+++ src/main/java/com/sparta/matchingservice/MatchingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.matchingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingServiceApplication.class, args);
+    }
+
+}

c62e0e9b887e600c38eeded2b60f60eecc412cfd
2h5i <dev.2h5i@gmail.com>
2023-01-18 15:51:25 +0900

    [Feat] 관리자 개발 : 서비스 개발 추가 및 TODO 추가

--- src/main/java/com/sparta/matchingservice/user/service/AdminService.java
+++ src/main/java/com/sparta/matchingservice/user/service/AdminService.java
@@ -2,16 +2,19 @@ package com.sparta.matchingservice.user.service;
 
 import com.sparta.matchingservice.user.dto.ResponseCustomersAdmin;
 import com.sparta.matchingservice.user.dto.ResponseSellerAdmin;
+import com.sparta.matchingservice.user.dto.SearchCustomersAdmin;
+import com.sparta.matchingservice.user.dto.SearchSellersAdmin;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AdminService {
 
-    Page<ResponseCustomersAdmin> getCustomersAdmin(Pageable pageable);
+    Page<ResponseCustomersAdmin> getCustomersAdmin(Pageable pageable, SearchCustomersAdmin searchCustomersAdmin);
 
-    Page<ResponseSellerAdmin> getSellerAdmin(Pageable pageable);
+    Page<ResponseSellerAdmin> getSellerAdmin(Pageable pageable, SearchSellersAdmin searchSellersAdmin);
 
-    Page<ResponseCustomersAdmin> getSellerEnrollmentCustomersAdmin(Pageable pageable);
+    // TODO : SELLER ENROLLMENT로 이동
+//    Page<ResponseCustomersAdmin> getSellerEnrollmentCustomersAdmin(Pageable pageable);
 
     void approveSellerEnrollment(Long customerId);
 
--- src/main/java/com/sparta/matchingservice/user/service/AdminServiceImpl.java
+++ src/main/java/com/sparta/matchingservice/user/service/AdminServiceImpl.java
@@ -2,6 +2,8 @@ package com.sparta.matchingservice.user.service;
 
 import com.sparta.matchingservice.user.dto.ResponseCustomersAdmin;
 import com.sparta.matchingservice.user.dto.ResponseSellerAdmin;
+import com.sparta.matchingservice.user.dto.SearchCustomersAdmin;
+import com.sparta.matchingservice.user.dto.SearchSellersAdmin;
 import com.sparta.matchingservice.user.entity.User;
 import com.sparta.matchingservice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,36 +16,40 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService{
 
-//    private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     @Override
-    public Page<ResponseCustomersAdmin> getCustomersAdmin(Pageable pageable) {
-//        Page<User> customers = userRepository.findAll(pageable);
+    public Page<ResponseCustomersAdmin> getCustomersAdmin(Pageable pageable, SearchCustomersAdmin searchCustomersAdmin) {
 
-
-
-
-        return null;
+        return userRepository.getCustomersByCondition(pageable, searchCustomersAdmin);
     }
 
+    @Transactional
     @Override
-    public Page<ResponseSellerAdmin> getSellerAdmin(Pageable pageable) {
-        return null;
+    public Page<ResponseSellerAdmin> getSellerAdmin(Pageable pageable, SearchSellersAdmin searchSellersAdmin) {
+        return userRepository.getSellersByCondition(pageable, searchSellersAdmin);
     }
 
-    @Override
-    public Page<ResponseCustomersAdmin> getSellerEnrollmentCustomersAdmin(Pageable pageable) {
-        return null;
-    }
+    // TODO : SellerEnrollment로 이동
+//    @Override
+//    public Page<ResponseCustomersAdmin> getSellerEnrollmentCustomersAdmin(Pageable pageable) {
+//        return null;
+//    }
 
+    @Transactional
     @Override
     public void approveSellerEnrollment(Long customerId) {
 
     }
 
+    @Transactional
     @Override
     public void deleteSellerAuthority(Long sellerId) {
-
+        User user = userRepository.findById(sellerId).orElseThrow(
+                () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
+        );
+        user.changeAuthoritySellerToCustomer();
+        userRepository.saveAndFlush(user);
     }
 }

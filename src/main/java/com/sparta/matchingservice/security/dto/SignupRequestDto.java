653b42e3781e8b3a885f78e2bc6414a4db1e765f
youngfromnowhere <graph1000limited@gmail.com>
2023-01-18 16:50:55 +0900

    [Refactor] Sign Up : Modify SignupRequestDto

--- src/main/java/com/sparta/matchingservice/security/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/matchingservice/security/dto/SignupRequestDto.java
@@ -9,11 +9,11 @@ import jakarta.validation.constraints.Size;
 @Setter
 @Getter
 public class SignupRequestDto {
-    @Size(min = 4, max = 10, message = "input 4-10 characters")
+    @Size(min = 4, max = 10, message = "User name should be 4-10 characters long")
     @Pattern(regexp = "[a-zA-Z0-9]+", message = "Special characters are not allowed.")
     private String userName;
 
-    @Size(min = 8, max = 15, message = "")
+    @Size(min = 8, max = 15, message = "Password should be 8-15 characters long")
     @Pattern(regexp = "[a-zA-Z0-9]+", message = "Special characters are not allowed.")
     private String password;
 }

fd8590b013bda8455bfe95272ad2ffbd3f18f43f
2h5i <dev.2h5i@gmail.com>
2023-01-19 10:15:01 +0900

    [Feat] 관리자 개발 : 판매자 등록 요청 폼 조회 및 판매자 권한 승인

--- src/main/java/com/sparta/matchingservice/sellerenrollment/service/SellerEnrollmentServiceImpl.java
+++ src/main/java/com/sparta/matchingservice/sellerenrollment/service/SellerEnrollmentServiceImpl.java
@@ -2,11 +2,14 @@ package com.sparta.matchingservice.sellerenrollment.service;
 
 import com.sparta.matchingservice.sellerenrollment.dto.ResponseSellerEnrollment;
 import com.sparta.matchingservice.sellerenrollment.dto.SearchSellerEnrollment;
+import com.sparta.matchingservice.sellerenrollment.entity.SellerEnrollment;
 import com.sparta.matchingservice.sellerenrollment.repository.SellerEnrollmentRepository;
+import com.sparta.matchingservice.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,12 +17,20 @@ public class SellerEnrollmentServiceImpl implements SellerEnrollmentService{
 
     private final SellerEnrollmentRepository sellerEnrollmentRepository;
 
+    @Transactional
     @Override
     public Page<ResponseSellerEnrollment> getSellerEnrollmentCustomersAdmin(SearchSellerEnrollment searchSellerEnrollment,
                                                                             Pageable pageable) {
 
-        sellerEnrollmentRepository.getSellerEnrollmentsByCondition(searchSellerEnrollment, pageable);
-
-        return null;
+        return sellerEnrollmentRepository.getSellerEnrollmentsByCondition(searchSellerEnrollment, pageable);
     }
+
+    @Transactional
+    @Override
+    public SellerEnrollment findSellerEnrollmentByCustomer(User customer) {
+        return sellerEnrollmentRepository.findByCustomer(customer).orElseThrow(
+                () -> new IllegalArgumentException("해당하는 등록 요청 폼이 없습니다.")
+        );
+    }
+
 }

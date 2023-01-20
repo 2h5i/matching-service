e1ac5eac922e4ba50511267ae6fdc4d1bbcda269
youngfromnowhere <graph1000limited@gmail.com>
2023-01-21 05:51:03 +0900

    Delete : BlackList entity, BlackListRedis Repository

--- src/main/java/com/sparta/matchingservice/security/repository/BlackListRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.matchingservice.security.repository;
-
-import com.sparta.matchingservice.security.entity.BlackList;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BlackListRedisRepository extends CrudRepository<BlackList, Long>{
-}
